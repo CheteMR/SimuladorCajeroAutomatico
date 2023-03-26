04853298dd6aff07c01ad0cef9ae87dcec78c7de
CheteMR <martinroque.ignacio@gmail.com>
2023-03-26 20:39:36 +0200

    Primer commit del cajero y readme

---
+++ SimuladorCajeroAutomatico/src/modelo/javabean/CajeroAutomatico.java
@@ -0,0 +1,26 @@
+package modelo.javabean;
+
+/**
+ * 
+ * @author CheteMR
+ * @version 1.0
+ * 
+ * Se realiza un pequeño cajero automático con las siguientes opciones:
+ * 
+ * 1. Ingresar
+ * 2. Retirar
+ * 3. Salir
+ */
+
+public class CajeroAutomatico {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ SimuladorCajeroAutomatico/src/modelo/javabean/README.md
@@ -0,0 +1,15 @@
+
+
+# Simulador Cajero Automático
+
+Se simula un pequeño cajero automático con tres opciones 
+para elegir. Las opciones son la siguientes: 
+
+**Ingresar**
+**Retirar**
+**Salir**
+
+## Nota
+
+Esta cuenta se va a crear con una constante **final** que nos dice que 
+dentro de la cuenta tenemos 1.000€. 
