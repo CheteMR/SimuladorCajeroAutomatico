895fdbff5f48a38f6e08a14f538e6fdf43fae0f7
CheteMR <martinroque.ignacio@gmail.com>
2023-03-26 22:34:44 +0200

    Se crea la estructura del cajero

--- SimuladorCajeroAutomatico/src/modelo/javabean/CajeroAutomatico.java
+++ SimuladorCajeroAutomatico/src/modelo/javabean/CajeroAutomatico.java
@@ -1,5 +1,5 @@
 package modelo.javabean;
-
+import java.util.Scanner;
 /**
  * 
  * @author CheteMR
@@ -15,6 +15,24 @@ package modelo.javabean;
 public class CajeroAutomatico {
 
 	public static void main(String[] args) {
+		
+		final int saldoInicial = 1000;
+		int opcion;
+		double ingreso, saldoActual, retirar;
+		
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("\nBienvenido a su cajero automático. ¿Qué quiere hacer?");
+		System.out.println("1. Ingresar dinero en la cuenta");
+		System.out.println("2. Retirar dinero de la cuenta");
+		System.out.println("3. Salir");
+		
+		opcion=entrada.nextInt();
+		
+		switch (opcion) {
+		case 1:
+		case 2: 
+		case 3:
+		}
 		
 		
 		
